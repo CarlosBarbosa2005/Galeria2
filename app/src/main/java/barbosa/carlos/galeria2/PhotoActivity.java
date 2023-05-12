2d46af97daadf65e1d303b3a96a60bdb360eef6b
20211tiimi0303 <cesbarbosa2005@gmail.com>
2023-05-12 08:58:36 -0300

    Galeria 2 12/05

---
+++ app/src/main/java/barbosa/carlos/galeria2/PhotoActivity.java
@@ -0,0 +1,14 @@
+package barbosa.carlos.galeria2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
