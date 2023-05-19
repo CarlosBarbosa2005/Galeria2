6a2b1a68df3c9d8f60df2eeb80feb7df5866f476
20211tiimi0303 <cesbarbosa2005@gmail.com>
2023-05-19 09:04:19 -0300

    Galeria 2 19/05

--- app/src/main/java/barbosa/carlos/galeria2/MainActivity.java
+++ app/src/main/java/barbosa/carlos/galeria2/MainActivity.java
@@ -3,14 +3,27 @@ package barbosa.carlos.galeria2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import barbosa.carlos.galeria2.Util.Util;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> photos = new ArrayList<>();
+
+    MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File [] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++){
+            photos.add(files[i].getAbsolutePath());
+        }
+
+        mainAdapter = new MainAdapter(MainActivity.this, photos);
+
+        RecyclerView rvGallery = findViewById(R.id.rvGallery);
+        rvGallery.setAdapter(mainAdapter);
+
+        float w = getResources().getDimension(R.dimen.itemWidth);
+        int numberOfColumns = Util.calculateNoOfColumns(MainActivity.this, w);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this,numberOfColumns);
+        rvGallery.setLayoutManager(gridLayoutManager);
     }
     @Override
     public boolean onCreateOptionsMenu (Menu menu){
@@ -34,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 dispatchTakePictureIntent();
                 return true;
             default:
+                return super.onOptionsItemSelected(item);
         }
 
     }
